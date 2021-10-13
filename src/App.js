7dfc0b6155009b69131ee315802ae2df06a30e9e
muthu <mkandanmk7@gmail.com>
2021-10-13 22:26:19 +0530

    Initialized App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Gmail from "./Components/Gmail";
+
+function App() {
+  return (
+    <div className="App">
+      <Gmail />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Gmail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gmail() {
+  return (
+    <div className="gmail">
+      <h2>Hello from gmail</h2>
+    </div>
+  );
+}
+
+export default Gmail;
