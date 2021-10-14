c4dc59a84f3c961493353a8f4b95fdcfdab19dd9
muthu <mkandanmk7@gmail.com>
2021-10-14 10:46:29 +0530

    side nav icons done

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,7 +4,13 @@ import "./css/Sidebar.css";
 //icons
 import InboxOutlinedIcon from "@material-ui/icons/InboxOutlined";
 import StarOutlined from "@material-ui/icons/StarOutlined";
-import WatchLaterIcon from "@material-ui/icons/WatchLaterIcon";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import SendIcon from "@material-ui/icons/Send";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+import LabelImportantSharpIcon from "@material-ui/icons/LabelImportantSharp";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import KeyboardIcon from "@material-ui/icons/Keyboard";
+import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
   return (
@@ -22,12 +28,45 @@ function Sidebar() {
         <div className="sidebarTopIcons">
           <StarOutlined />
         </div>
-        {/* <div className="sidebarTopIcons">
+        <div className="sidebarTopIcons">
           <WatchLaterIcon />
-        </div> */}
-        <div className="sidebarTopIcons"></div>
-        <div className="sidebarTopIcons"></div>
-        <div className="sidebarTopIcons"></div>
+        </div>
+        <div className="sidebarTopIcons">
+          <SendIcon />
+        </div>
+        <div className="sidebarTopIcons">
+          <InsertDriveFileIcon />
+        </div>
+        <div className="sidebarTopIcons">
+          <LabelImportantSharpIcon />
+        </div>
+      </div>
+      <div className="sidebarBottom">
+        <div className="sidebarOption">
+          <img
+            src="https://www.gstatic.com/images/icons/material/system/1x/meet_black_20dp.png"
+            alt="meetIcon"
+          />
+        </div>
+        <div className="sidebarOption">
+          <VideocamIcon />
+        </div>
+        <div className="sidebarOption">
+          <KeyboardIcon />
+        </div>
+      </div>
+      <div className="sidebarLast">
+        <div className="sidebarLastOption">
+          <div className="sidebarOption">
+            <img
+              src="https://www.gstatic.com/images/icons/material/system/1x/hangout_black_20dp.png"
+              alt="hangoutIcon"
+            />
+          </div>
+          <div className="sidebarAvatar">
+            <Avatar />
+          </div>
+        </div>
       </div>
     </div>
   );
